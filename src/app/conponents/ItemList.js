cdd436ea8a1eb4b69f7f6b5cca7ae132a4517506
FajnyKuba784 <paruwka150@gmail.com>
2024-09-17 21:08:15 +0200

    ok

---
+++ src/app/conponents/ItemList.js
@@ -0,0 +1,6 @@
+function ItemList({ items }) {
+    return (
+      <ul>      {items.map((item, index) => (        <li key={index}>{item}</li>
+        ))}
+      </ul>  );
+  }export default ItemList;
