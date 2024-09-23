412d8ce443860cfe3099cfd32e1a873cb8843842
FajnyKuba784 <paruwka150@gmail.com>
2024-09-23 19:08:31 +0200

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import ItemList from "./conponents/ItemList.js";
 import TextBlock from "./conponents/TextBlock.js";
 import ImageWithText from "./conponents/ImageWithText.js";
-
-
+import ItemList from "./conponents/ItemList.js";
+import SimpleForm from "./conponents/SimpleForm";
+import ConditionalDisplay from "./conponents/ConditionalDisplay";
+import ListWithObjects from "./conponents/ListWithObjects";
 
 
 
@@ -14,9 +15,12 @@ export default function Home() {
       <main className={styles.main}>
 
       <a>Jakub Jackiewicz</a>
-      <ItemList items={["ada","dwad"]}/> 
-      <TextBlock content={"sigma"} heading={"jakub"}/>
+      <ItemList items={["jeden","dwa"]}/> 
+      <TextBlock content={"j"} heading={"jakub"}/>
       <ImageWithText text={"auto"} imageUrl={"https://beesafe.pl/porady/wp-content/uploads/2022/09/najszybsze-auto-na-swiecie.jpg"}/>
+      <SimpleForm label={"input"} placeholder={"TEXT"}/>
+      <ConditionalDisplay isVisible={false}/>
+      <ListWithObjects items={[{name : "pierwsza" ,value :"1"}]}/>
       </main>
     </div>
   );
